f5e71038e0847ac4f9dea81fa1018fe483ff0b69
wmfsljy-coder <wmfsljy@gmail.com>
2026-05-13 15:39:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.set_page_config(page_title="공문서 교정기", layout="wide")
 st.title("📄 공문서 교정기")
 st.caption("선생님의 업무 경감을 위한 프로그램입니다.")
 
-col1, col2 = st.columns(2)
+col1, col2, co13 = st.columns(2)
 
 with col1:
     st.subheader("📥 검토할 내용")
